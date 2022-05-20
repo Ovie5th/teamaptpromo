42c921b2e7392d809668cee0e24ff6b2221aa8c9
Ovie5th <ovie5th@gmail.com>
2022-05-21 00:42:27 +0100

    shooting star added

--- src/components/bottomContent/Bottom.jsx
+++ src/components/bottomContent/Bottom.jsx
@@ -5,8 +5,6 @@ import coinright from "../../images/coinright.png";
 import star1 from "../../images/star1.png";
 import star2 from "../../images/star2.png";
 import star3 from "../../images/star3.png";
-import { fadeOutUpBig } from "react-animations";
-import styled, { keyframes } from "styled-components";
 import "./bottom.css";
 
 export default function Bottom() {
@@ -19,17 +17,11 @@ export default function Bottom() {
         <img className="star1" src={star1} alt="" />
         <img className="star2" src={star2} alt="" />
         <img className="star3" src={star3} alt="" />
-        <ShootingStars className="star4" src={star1} alt="" />
-        <ShootingStars className="star4" src={star2} alt="" />
-        <ShootingStars className="star4" src={star3} alt="" />
+        <img className="shootingstar one" src={star3} alt="" />
+        <img className="shootingstar two" src={star3} alt="" />
+        <img className="shootingstar three" src={star3} alt="" />
+        <img className="shootingstar four" src={star3} alt="" />
       </div>
     </div>
   );
 }
-
-
-const FadeOutAnimation = keyframes`${fadeOutUpBig}`;
-
-const ShootingStars = styled.img`
-  animation: 20s ${FadeOutAnimation};
-`;
